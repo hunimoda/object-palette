aff79f5557259968ac42a758904c42c5cf0a8d51
Dae Hoon Kim <hunimoda@gmail.com>
2022-01-12 14:55:31 +0900

    Added header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,11 @@
 import classes from "./Header.module.css";
 
 const Header = () => {
-	return <header className={classes.header}></header>;
+	return (
+		<header className={classes.header}>
+			<h1>Navilera</h1>
+		</header>
+	);
 };
 
 export default Header;
